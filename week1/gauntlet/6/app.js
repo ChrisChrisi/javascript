e1ff0b1f963a7bbdd82da9d66bbdad685a3b4951
ChrisChrisi <kristina_milev@abv.bg>
2014-06-10 12:33:50 +0300

    solved gauntlet6

--- week1/gauntlet/6/app.js
+++ week1/gauntlet/6/app.js
@@ -1,3 +1,31 @@
-/**
- * Created by John Smith on 6/10/2014.
- */
+"use strict";
+
+var insterHtmlTag = function (content, tag) {
+    return "<" + tag + ">" + content + "</" + tag + ">";
+};
+
+var moveOptions = function (from, to) {
+    var selectedItems = $(from + " option:selected");
+    selectedItems.each(function () {
+        var input = insterHtmlTag($(this).text(), "option");
+        $(this).remove();
+        $(to).append(input);
+    });
+};
+$(document).ready(function () {
+    $.getJSON('http://localhost:3000/items', function (items, textStatus) {
+        items.forEach(function (item) {
+            var input = insterHtmlTag(item, "option");
+            $("#select-left").append(input);
+        });
+    });
+
+    $("#to-right").on("click", function () {
+        moveOptions("#select-left", "#select-right");
+    });
+
+    $("#to-left").on("click", function () {
+        moveOptions("#select-right", "#select-left");
+    });
+
+});
--- week1/gauntlet/6/server/server.js
+++ week1/gauntlet/6/server/server.js
@@ -5,54 +5,25 @@
 var express = require('express');
 var app = express();
 
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
 });
 
-var students = [{
-  "id" : 1,
-  "name" : "Daniel Taskoff",
-  "course" : "Frontend JavaScript",
-  "github" : "https://github.com/dtaskoff/"
-}, {
-  "id" : 2,
-  "name" : "Elena Jeleva",
-  "course" : "Programming 101",
-  "github" : "https://github.com/egzheleva/JavaScript"
-}, {
-  "id" : 3,
-  "name" : "Luboslava Dimitrova",
-  "course" : "Frontend JavaScript",
-  "github" : "https://github.com/LucyDimitrova"
-}, {
-  "id": 4,
-  "name" : "Anton Antonov",
-  "course" : "Core Java",
-  "github" : "https://github.com/syndbg"
-}, {
-  "id" : 5,
-  "name" : "Nikola Dichev",
-  "course" : "Core Java",
-  "github" : "https://github.com/kokod"
-}, {
-  "id" : 6,
-  "name" : "Ivaylo Bachvaroff",
-  "course" : "Frontend JavaScript",
-  "github" : "https://github.com/Ivaylo-Bachvarov"
-}, {
-  "id" : 7,
-  "name" : "Emanuela Mollova",
-  "course" : "Frontend JavaScript",
-  "github" : "https://github.com/EmanuelaMollova"
-}];
+var items = [
+    "Item 1",
+    "Item 2",
+    "Item 3",
+    "Item 4",
+    "Item 5",
+    "Item 6"
+];
 
-app.get('/students', function(req, res){
-  res.json(students);
+app.get('/items', function (req, res) {
+    res.json(items);
 });
 
-
-var server = app.listen(3000, function() {
+var server = app.listen(3000, function () {
     console.log('Listening on port %d', server.address().port);
 });
